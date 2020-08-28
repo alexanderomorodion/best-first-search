cc6c4999c43a2945b9c40ff2f8fa6f998ac02e5e
Alexander Omorodion <aiomcy@umsystem.edu>
2020-08-28 15:29:23 -0500

    Added relevant files Began populating adjacency map

--- Best-First-Search/Best-First-Search.cpp
+++ Best-First-Search/Best-First-Search.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -10,8 +15,33 @@ double calculateDistance(double initX, double initY, double finalX, double final
     return sqrt(xTerm + yTerm);
 }
 
+void populateMap(map<string, vector<string>> cityMap)
+{
+    stringstream ss;
+    string adjLine;
+    string locLine;
+    ifstream adjFile;
+    ifstream locFile;
+    adjFile.open("Adjacencies.txt.");
+
+    if (adjFile.is_open())
+    {
+        while (getline(adjFile, adjLine))
+        {
+            ss << adjLine;
+            string keyCity = adjLine[0];
+            
+        }
+
+
+        adjFile.close();
+    }
+}
+
 int main()
 {
-    cout << calculateDistance(-7, -4, 17, 6.5);
+    
+    map<string, vector<string>> cityMap;
+    populateMap(cityMap);
 }
 
