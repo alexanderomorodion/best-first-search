28929a41284205ef687855de6c67e46d71cfcc3e
Alexander Omorodion <aiomcy@umsystem.edu>
2020-08-28 14:51:45 -0500

    Created city class

---
+++ Best-First-Search/City.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class City 
+{
+	private:
+		bool visited;
+		string name;
+		string previousCity;
+		double distanceToDest;
+		vector<string> adjacentCities;
+		
+
+
+	public:
+
+};
