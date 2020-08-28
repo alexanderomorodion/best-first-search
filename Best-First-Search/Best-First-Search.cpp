4b556fc1bb83881b74da96c23005aab88664b688
Alexander Omorodion <aiomcy@umsystem.edu>
2020-08-28 15:00:30 -0500

    Added distance formula

--- Best-First-Search/Best-First-Search.cpp
+++ Best-First-Search/Best-First-Search.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+double calculateDistance(double initX, double initY, double finalX, double finalY)
+{
+    double xTerm = pow((finalX - initX), 2);
+    double yTerm = pow((finalY - initY), 2);
+    return sqrt(xTerm + yTerm);
+}
+
 int main()
 {
-    
+    cout << calculateDistance(-7, -4, 17, 6.5);
 }
 
