ab0b7b68e81f4eb56d824c84f53b887f7c91151d
Alex Omorodion <alexanderomorodion@gmail.com>
2020-08-29 13:30:22 -0500

    Successfully populated adjacency map

--- Best-First-Search/Best-First-Search.cpp
+++ Best-First-Search/Best-First-Search.cpp
@@ -15,7 +15,7 @@ double calculateDistance(double initX, double initY, double finalX, double final
     return sqrt(xTerm + yTerm);
 }
 
-void populateMap(map<string, vector<string>> cityMap)
+void populateMap(map<string, vector<string>>& cityMap)
 {
     stringstream ss;
     string adjLine;
@@ -24,12 +24,64 @@ void populateMap(map<string, vector<string>> cityMap)
     ifstream locFile;
     adjFile.open("Adjacencies.txt.");
 
+    vector<string> currLine;
+
     if (adjFile.is_open())
     {
         while (getline(adjFile, adjLine))
         {
-            ss << adjLine;
-            string keyCity = adjLine[0];
+            //
+            string city = "";
+            for (int i = 0; i < adjLine.length(); i++)
+            {
+                if (adjLine[i] == ' ' && city.length() == 0)
+                {
+                    continue;
+                }
+
+                else if (adjLine[i] == ' ')
+                {
+                    currLine.push_back(city);
+                    city = "";
+                    continue;
+                }
+
+                else
+                    city += adjLine[i];
+            }
+
+            if (city.find_first_not_of(' ') != string::npos)
+            {
+                currLine.push_back(city);
+            }   
+
+            for (int i = 0; i < currLine.size(); i++) 
+            {
+                vector<string> restOfVecBefore = vector<string>(currLine.begin(), currLine.end() - currLine.size() + i);
+                vector<string> restOfVecAfter = vector<string>(currLine.begin() + i + 1, currLine.end());
+                vector<string> restOfVec;
+                restOfVec.reserve(restOfVecBefore.size() + restOfVecAfter.size());
+                restOfVec.insert(restOfVec.end(), restOfVecAfter.begin(), restOfVecAfter.end());
+                restOfVec.insert(restOfVec.end(), restOfVecBefore.begin(), restOfVecBefore.end());
+
+                if (cityMap.find(currLine[i]) == cityMap.end()) 
+                {
+                    //create new entry in vector, and add adjacent cities
+                    cityMap[currLine[i]] = restOfVec;
+                }
+                else 
+                {
+                    //add new adjacent cities of existing entry to vector
+                    vector<string> copyVector;
+                    copyVector.reserve(cityMap[currLine[i]].size() + restOfVec.size());
+                    copyVector.insert(copyVector.end(), cityMap[currLine[i]].begin(), cityMap[currLine[i]].end());
+                    copyVector.insert(copyVector.end(), restOfVec.begin(), restOfVec.end());
+                    cityMap[currLine[i]] = copyVector;
+                }
+                
+            }
+
+            currLine.clear();
             
         }
 
@@ -43,5 +95,15 @@ int main()
     
     map<string, vector<string>> cityMap;
     populateMap(cityMap);
+    for (auto city : cityMap)
+    {
+        cout << city.first << " is adjacent to: ";
+        for (int i = 0; i < city.second.size(); i++)
+        {
+            cout << city.second[i] << ", ";
+        }
+
+        cout << endl << endl << endl;
+    }
 }
 
